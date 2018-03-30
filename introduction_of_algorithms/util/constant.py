91f3ac752b298c490d9e33c833eec339878f6d9f
mangonob <mangonob@qq.com>
2018-03-30 18:00:37 +0800

    Prepare remove NIL

--- introduction_of_algorithms/util/constant.py
+++ introduction_of_algorithms/util/constant.py
@@ -1,29 +1,6 @@
 import math
 
 
-def shared_nil():
-    class Nil(object):
-        """ Provide a sentinel object. """
-
-        def __call__(self, *args, **kwargs):
-            return self
-
-        def __getattr__(self, item):
-            return self
-
-        def __bool__(self):
-            return False
-
-        def __str__(self):
-            return "nil"
-
-    if not hasattr(Nil, "shared"):
-        Nil.shared = Nil()
-
-    return Nil.shared
-
-NIL = shared_nil()
-
 PHI = (math.sqrt(5) - 1) / 2
 
 
